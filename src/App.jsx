9182e05f81bafe92ee38dd021a81d7477257a3d8
Radames-prog <easycompvila@gmail.com>
2021-10-22 10:27:48 -0300

    Adicionado componentes condicional

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,32 @@ import Primeiro from './components/Primeiro'
 import ComParametro from './components/ComParametro'
 import ComFilhos from'./components/ComFilhos'
 import Card from'./components/Layout/Card'
+import Repeticao from'./components/Repeticao'
+import Condicional from'./components/Condicional'
+import CondicionalComIf from'./components/CondicionalComIf'
+
 
 export default (props)=>(
     <div className="App">
-        <Card titulo="Primeiro Componente">
+        <Card titulo="#5 - Condicional v1">
+            <Condicional numero={1}></Condicional>
+        </Card>
+        <Card titulo="#6 - Condicional v2">
+            <CondicionalComIf numero={1}></CondicionalComIf>
+        </Card>
+        <Card titulo="#4 - Repetição">
+            <Repeticao></Repeticao>
+        </Card>
+        <Card titulo="#1 Primeiro Componente">
             <Primeiro/>
         </Card>
-        <Card titulo="Componente com Parâmetro">
+        <Card titulo="#2 Componente com Parâmetro">
             <ComParametro 
             titulo="Entrando no mundo do React"
             subtitulo="Esta complicado mas vamos la!!"/>
             
         </Card>
-        <Card titulo="Componente com filho">
+        <Card titulo="#3 Componente com filho">
             <ComFilhos>
             <ul>
                 <li>Ana</li>
